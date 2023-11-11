74652eecf17c354925f7e16752331bfc5ab72e37
Arjun Kaura <arjunkaura@hotmail.com>
2023-11-11 14:14:51 -0500

    added import

--- Bezos/src/main/java/com/app/LoginServlet.java
+++ Bezos/src/main/java/com/app/LoginServlet.java
@@ -15,6 +15,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.app.DatabaseConnection;
+
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
