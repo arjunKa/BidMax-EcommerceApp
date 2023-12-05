165e79fee7673ac54a7ca077ebe8af4d90a40499
arjunKa <32141878+arjunKa@users.noreply.github.com>
2023-12-05 11:04:15 -0500

    springboot

---
+++ Bezos/src/main/java/com/app/Bezos/BezosApplication.java
@@ -0,0 +1,13 @@
+package com.app.Bezos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezosApplication.class, args);
+	}
+
+}
---
+++ Bezos_sb/src/test/java/com/app/Bezos/BezosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Bezos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
